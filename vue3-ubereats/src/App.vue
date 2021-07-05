f564234dc12c7376d38ada9b4148bbdecf265c99
PepseeActus <christophe.sbastien@gmail.com>
2021-07-05 23:44:10 +0200

    intallation de l'environnement

---
+++ vue3-ubereats/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <restaurant-card></restaurant-card>
+</template>
+
+<script>
+import RestaurantCard from './components/RestaurantCard.vue'
+export default {
+  name: 'App',
+  components: {
+    RestaurantCard
+  }
+}
+</script>
+
+<style>
+@import url('./style/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+#app {
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-ubereats/src/components/RestaurantCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="restaurant--card">
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.restaurant--card {
+    width: 32%;
+    height: 30vh;
+}
+
+</style>
